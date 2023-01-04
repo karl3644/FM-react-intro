acfd84b53f70fb9d677e3539787ff1333c377c60
karl3644 <43153810+karl3644@users.noreply.github.com>
2023-01-04 20:05:03 +0000

    node and express server to serve basic split html

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { Express } from "express";
+import express from "express";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -11,3 +11,34 @@ const PORT = process.env.PORT || 3001;
 const html = fs
   .readFileSync(path.resolve(__dirname, "./dist/client/index.html"))
   .toString();
+
+const parts = html.split("not rendered");
+
+const app = express();
+app.use(
+  "/assets",
+  express.static(path.resolve(__dirname, "./dist/client/assets"))
+);
+
+app.use((req, res) => {
+  res.write(parts[0]);
+
+  const stream = renderApp(req.url, {
+    onShellReady() {
+      stream.pipe(res);
+    },
+    onShellError() {
+      //  do error handling here
+    },
+    onAllReady() {
+      res.write(parts[1]);
+      res.end();
+    },
+    onError(err) {
+      console.error(err);
+    },
+  });
+});
+
+console.log(`listening on http://localhost:${PORT}`);
+app.listen(PORT);
--- src/ServerApp.jsx
+++ src/ServerApp.jsx
@@ -2,7 +2,7 @@ import { renderToPipeableStream } from "react-dom/server";
 import { StaticRouter } from "react-router-dom/server";
 import App from "./App";
 
-export default function render(url, opts) {
+export default function renderApp(url, opts) {
   const stream = renderToPipeableStream(
     <StaticRouter location={url}>
       <App />
